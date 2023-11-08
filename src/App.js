3fa5a1df014d3740692894c2cdf0aae3430c753c
grolland <ilya.play111@gmail.com>
2023-11-08 22:40:41 +0300

    ready project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Block } from './Block';
+import './index.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <Block value={0} currency="RUB" />
+            <Block value={0} currency="USD" />
+        </div>
+    );
+}
+
+export default App;
